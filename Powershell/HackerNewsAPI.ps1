9a07aec8221740cb8a1813bb09fb9fbef6219c54
David Richmond <11792887+celadyn@users.noreply.github.com>
2025-03-16 16:43:00 -0500

    Update HackerNewsAPI.ps1
    add lookback window modification, better verbose output, Brief mode

--- Powershell/HackerNewsAPI.ps1
+++ Powershell/HackerNewsAPI.ps1
@@ -23,47 +23,72 @@ function Get-HNStoriesWithCommentsMatching {
     ♪♫♪ created by david richmond ♪♫♪
 
     #>
-
+    [CmdletBinding()]
     param (
         $QueryString
         ,
-        [int]$LookbackDays = 7
+        [int]$LookbackAnchorDaysAgo
+        ,
+        [int]$LookbackDaysAgo = 7
+        ,
+        [switch]$Brief
     )
 
-    $StartDate = (Get-Date).AddDays(-$LookbackDays)
-
-    $UnixTimestamp = ([datetimeoffset]($StartDate)).ToUnixTimeSeconds()
-
-    $HNAPIResponse = Invoke-RestMethod "http://hn.algolia.com/api/v1/search_by_date?tags=comment&query=$($QueryString)&numericFilters=created_at_i>$($UnixTimestamp)"
-
-    $HNComments = $HNAPIResponse.hits
-
-    #$HNComments | select updated_at,author,story_title,story_id,comment_text,objectID,parent_id | sort updated_at | Format-List
-
-    $HNComments | % {
-        $_| Add-Member -MemberType NoteProperty -Name comment_link -Value "https://news.ycombinator.com/item?id=$($_.objectID)"
-    }
-
-    $HNStoryGroups = $HNComments | group story_id
-
-
-    $StorySummaries = foreach ($HNStoryGroup in $HNStoryGroups) {
     
-        $StoryTitle = $HNStoryGroup.Group[0].story_title
-        [string]$StoryID = $HNStoryGroup.Group[0].story_id
-        [string]$StoryEarliestComment = $HNStoryGroup.Group | sort -property created_at -desc | select -first 1 | select -expandproperty created_at
-        $StoryLink = "https://news.ycombinator.com/item?id=$StoryID"
-
-        [pscustomobject]@{
-            story_title = $StoryTitle
-            story_id = $StoryID
-            story_link = $StoryLink
-            story_earliest_comment = $StoryEarliestComment
-            comments = [pscustomobject]@{hits=$HNStoryGroup.Group}
-            commentcount = $HNStoryGroup.count
-        }
+    $EndDate = if ($null -ne $LookbackAnchorDaysAgo -and $LookbackAnchorDaysAgo -gt 0) {
+        (Get-Date).AddDays(-$LookbackAnchorDaysAgo)
+    } else {
+        (Get-Date)
     }
 
-    $StorySummaries
+    $StartDate = $EndDate.AddDays(-$LookbackDaysAgo)
+    Write-Debug "Looking back from $($StartDate) to $($EndDate)"
 
+    $StartUnixTimestamp = ([datetimeoffset]($StartDate)).ToUnixTimeSeconds()
+    $EndUnixTimestamp = ([datetimeoffset]($EndDate)).ToUnixTimeSeconds()
+    
+
+    $Uri = "http://hn.algolia.com/api/v1/search_by_date?tags=comment&query=$($QueryString)&numericFilters=created_at_i>$($StartUnixTimestamp),created_at_i<$($EndUnixTimestamp)"
+    Write-Debug $Uri
+    
+    $HNAPIResponse = Invoke-RestMethod -Uri $Uri
+    if ($HNAPIResponse.hits.Count -eq 0) {
+        Write-Warning "No results found for $($QueryString) in the last $($LookbackDaysAgo) days"
+
+    } else {
+
+        $HNComments = $HNAPIResponse.hits
+        Write-Host -ForegroundColor Green "Found $($HNComments.Count) comments for $($QueryString) in the $($LookbackDaysAgo) days before $($EndDate)"
+        #$HNComments | select updated_at,author,story_title,story_id,comment_text,objectID,parent_id | sort updated_at | Format-List
+
+        $HNComments | % {
+            $_| Add-Member -MemberType NoteProperty -Name comment_link -Value "https://news.ycombinator.com/item?id=$($_.objectID)"
+        }
+
+        $HNStoryGroups = $HNComments | group story_id
+
+
+        $StorySummaries = foreach ($HNStoryGroup in $HNStoryGroups) {
+        
+            $StoryTitle = $HNStoryGroup.Group[0].story_title
+            [string]$StoryID = $HNStoryGroup.Group[0].story_id
+            [string]$StoryEarliestComment = $HNStoryGroup.Group | sort -property created_at -desc | select -first 1 | select -expandproperty created_at
+            $StoryLink = "https://news.ycombinator.com/item?id=$StoryID"
+
+            [pscustomobject]@{
+                story_title = $StoryTitle
+                story_id = $StoryID
+                story_link = $StoryLink
+                story_earliest_comment = $StoryEarliestComment
+                comments = [pscustomobject]@{hits=$HNStoryGroup.Group}
+                commentcount = $HNStoryGroup.count
+            }
+        }
+
+        if ($Brief) {
+            $StorySummaries.comments.hits | Select-Object story_title,story_url,created_at,author,comment_text,comment_link
+        } else {
+            $StorySummaries
+        }
+    }#if-responses
 }
